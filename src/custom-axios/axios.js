b778412b1152648e8c0fc89c5487b9ed8397e44a
andrej286 <mikistojanovski6@gmial.com>
2022-04-25 20:16:08 +0200

    final final commit

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://e-library-emit-lab1.herokuapp.com/ap',
+    baseURL: 'https://e-library-emit-lab1.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
